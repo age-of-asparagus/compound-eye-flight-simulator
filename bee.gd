764da55b33e9186357ad4af6e1cf44366af544c1
Tylere Couture <tylere.couture@gmail.com>
2025-07-27 07:41:12 -0700

    Fic Game Over Bug; Fix arrow controls

--- bee.gd
+++ bee.gd
@@ -88,6 +88,6 @@ func _on_hive_detector_area_entered(area: Area3D) -> void:
 	necter_sound.play()
 	Global.points += Global.necter
 	Global.necter = 0
-	
 	if Global.points >= Global.points_max:
+		print("WINNING!ddd")
 		get_tree().change_scene_to_file("res://Menus/win_menu.tscn")
